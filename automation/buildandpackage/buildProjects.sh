223510025f3f81fb79dab29c57296915b0e0f159
jules <cdaf@outlook.co.nz>
2018-10-02 14:25:36 +1300

    Trial upgrade

--- automation/buildandpackage/buildProjects.sh
+++ automation/buildandpackage/buildProjects.sh
@@ -1,50 +1,50 @@
 #!/usr/bin/env bash
 
-echo
-echo "$0 : +----------------------------+"
-echo "$0 : | Process BUILD all projects |"
-echo "$0 : +----------------------------+"
-echo
+scriptName=${0##*/}
+
+echo; echo "$scriptName : +----------------------------+"
+echo "$scriptName : | Process BUILD all projects |"
+echo "$scriptName : +----------------------------+"; echo
 SOLUTION="$1"
 if [ -z "$SOLUTION" ]; then
-	echo "$0 : Solution not passed!"
+	echo "$scriptName : Solution not passed!"
 	exit 1
 else
-	echo "$0 :   SOLUTION       : $SOLUTION"
+	echo "$scriptName :   SOLUTION         : $SOLUTION"
 fi
 
 BUILDNUMBER="$2"
 if [ -z "$BUILDNUMBER" ]; then
-	echo "$0 : Build Number not passed!"
+	echo "$scriptName : Build Number not passed!"
 	exit 2
 else
-	echo "$0 :   BUILDNUMBER    : $BUILDNUMBER"
+	echo "$scriptName :   BUILDNUMBER      : $BUILDNUMBER"
 fi
 
 REVISION="$3"
 if [ -z "$REVISION" ]; then
 	REVISION="Revision"
-	echo "$0 :   REVISION       : $REVISION (default)"
+	echo "$scriptName :   REVISION         : $REVISION (default)"
 else
-	echo "$0 :   REVISION       : $REVISION"
+	echo "$scriptName :   REVISION         : $REVISION"
 fi
 
 ACTION="$4"
 if [ -z "$ACTION" ]; then
-	echo "$0 :   ACTION         : $ACTION"
+	echo "$scriptName :   ACTION           : $ACTION"
 	BUILDENV='BUILDER'
-	echo "$0 :   BUILDENV       : $BUILDENV (default because ACTION not supplied)"
+	echo "$scriptName :   BUILDENV         : $BUILDENV (default because ACTION not supplied)"
 else
 	# case insensitive by forcing to uppercase
 	testForClean=$(echo "$ACTION" | tr '[a-z]' '[A-Z]')
 	if [ "$testForClean" == "CLEAN" ]; then
-		echo "$0 :   ACTION         : $ACTION (Build Environment will be set to default)"
+		echo "$scriptName :   ACTION           : $ACTION (Build Environment will be set to default)"
 		BUILDENV='BUILDER'
-		echo "$0 :   BUILDENV       : $BUILDENV (default)"
+		echo "$scriptName :   BUILDENV         : $BUILDENV (default)"
 	else
 		BUILDENV="$ACTION"
-		echo "$0 :   ACTION         : $ACTION"
-		echo "$0 :   BUILDENV       : $BUILDENV (derived from action)"
+		echo "$scriptName :   ACTION           : $ACTION"
+		echo "$scriptName :   BUILDENV         : $BUILDENV (derived from action)"
 	fi
 fi
 
@@ -53,16 +53,14 @@ for i in $(find . -mindepth 1 -maxdepth 1 -type d); do
 	directoryName=${i%%/}
 	if [ -f "$directoryName/CDAF.linux" ] ; then
 		AUTOMATIONROOT="$directoryName"
-		echo "$0 :   AUTOMATIONROOT : $AUTOMATIONROOT (CDAF.linux found)"
+		echo "$scriptName :   AUTOMATIONROOT   : $AUTOMATIONROOT (CDAF.linux found)"
 	fi
 done
 if [ -z "$AUTOMATIONROOT" ]; then
 	AUTOMATIONROOT="automation"
-	echo "$0 :   AUTOMATIONROOT : $AUTOMATIONROOT (CDAF.linux not found)"
+	echo "$scriptName :   AUTOMATIONROOT   : $AUTOMATIONROOT (CDAF.linux not found)"
 fi
 
-echo
-echo "$0 : $automessage"
 AUTOMATIONHELPER="$AUTOMATIONROOT/remote"
 
 # Check for user defined solution folder, i.e. outside of automation root, if found override solution root
@@ -70,19 +68,68 @@ SOLUTIONROOT="$AUTOMATIONROOT/solution"
 for i in $(find . -mindepth 1 -maxdepth 1 -type d); do
 	directoryName=${i%%/}
 	if [ -f "$directoryName/CDAF.solution" ]; then
-		echo "$0 : CDAF.solution file found in directory $directoryName, load solution properties"
 		SOLUTIONROOT="$directoryName"
-		propertiesList=$($AUTOMATIONHELPER/transform.sh "$directoryName/CDAF.solution")
-		echo
-		echo "$propertiesList"
-		eval $propertiesList
 	fi
 done
 
+printf "$scriptName :   Properties Driver : "
+propertiesDriver="$SOLUTIONROOT/properties.cm"
+if [ -f $propertiesDriver ]; then
+	echo "found ($propertiesDriver)"
+else
+	echo "none ($propertiesDriver)"
+fi
+
+echo; echo "$scriptName : CDAF.solution file found in directory $SOLUTIONROOT, load solution properties"
+if [ -f $SOLUTIONROOT/CDAF.solution ]; then
+	propertiesList=$($AUTOMATIONHELPER/transform.sh "$SOLUTIONROOT/CDAF.solution")
+	echo; echo "$propertiesList"
+	eval $propertiesList
+else
+	echo; echo "$scriptName : CDAF.solution file not found!"; exit 8823
+fi
+
+echo; echo "$scriptName : Remove working directories"; echo # perform explicit removal as rm -rfv is too verbose
+for packageDir in $(echo "./propertiesForRemoteTasks ./propertiesForLocalTasks"); do
+	if [ -d  "${packageDir}" ]; then
+		echo "  removed ${packageDir}"
+		rm -rf ${packageDir}
+	fi
+done
+
+# Properties generator (added in release 1.7.8)
+if [ -f $propertiesDriver ]; then
+	echo; echo "$scriptName : Generating properties files from ${propertiesDriver}"
+	header=$(head -n 1 ${propertiesDriver})
+	read -ra columns <<<"$header"
+	config=$(tail -n +2 ${propertiesDriver})
+	while read -r line; do
+		read -ra arr <<<"$line"
+		if [[ "${arr[0]}" == 'remote' ]]; then
+			cdafPath="./propertiesForRemoteTasks"
+		else
+			cdafPath="./propertiesForLocalTasks"
+		fi
+		echo "$scriptName : Generating ${cdafPath}/${arr[1]}"
+		if [ ! -d ${cdafPath} ]; then
+			mkdir -p ${cdafPath}
+		fi
+		for i in "${!columns[@]}"; do
+			if [ $i -gt 1 ]; then # do not create entries for context and target
+				echo "${columns[$i]}=${arr[$i]}" >> "${cdafPath}/${arr[1]}"
+			fi
+		done
+	done < <(echo "$config")
+	if [ -d "$SOLUTIONROOT/propertiesForRemoteTasks" ] && [ -d "./propertiesForRemoteTasks/" ]; then
+		echo "$scriptName : Generated properties will be merged with any defined properties in $SOLUTIONROOT/propertiesForRemoteTasks"
+	fi
+	if [ -d "$SOLUTIONROOT/propertiesForLocalTasks" ] && [ -d "./propertiesForLocalTasks/" ]; then
+		echo "$scriptName : Generated properties will be merged with any defined properties in $SOLUTIONROOT/propertiesForLocalTasks"
+	fi
+fi
+
 if [ -f "build.sh" ]; then
-	echo
-	echo "$0 : build.sh found in solution root, executing in $(pwd)"
-	echo
+	echo; echo "$scriptName : build.sh found in solution root, executing in $(pwd)"; echo
 	# Additional properties that are not passed as arguments, but loaded by execute automatically
 	echo "PROJECT=$PROJECT" > ./solution.properties
 	echo "REVISION=$REVISION" >> ./solution.properties
@@ -91,16 +138,14 @@ if [ -f "build.sh" ]; then
 	./build.sh "$SOLUTION" "$BUILDNUMBER" "$REVISION" "$BUILDENV" "$ACTION"
 	exitCode=$?
 	if [ $exitCode -ne 0 ]; then
-		echo "$0 : $PROJECT Build Failed, exit code = $exitCode."
+		echo "$scriptName : $PROJECT Build Failed, exit code = $exitCode."
 		exit $exitCode
 	fi
 fi
 	
 if [ -f "build.tsk" ]; then
 
-	echo
-	echo "$0 : build.tsk found in solution root, executing in $(pwd)"
-	echo
+	echo; echo "$scriptName : build.tsk found in solution root, executing in $(pwd)"; echo
 	# Additional properties that are not passed as arguments, explicit load is required
 	echo "PROJECT=$PROJECT" > ./solution.properties
 	echo "REVISION=$REVISION" >> ./solution.properties
@@ -109,7 +154,7 @@ if [ -f "build.tsk" ]; then
 	$AUTOMATIONHELPER/execute.sh "$SOLUTION" "$BUILDNUMBER" "$BUILDENV" "build.tsk" "$ACTION" 2>&1
 	exitCode=$?
 	if [ $exitCode -ne 0 ]; then
-		echo "$0 : Linear deployment activity ($AUTOMATIONHELPER/execute.sh $SOLUTION $BUILDNUMBER $PROJECT build.tsk) failed! Returned $exitCode"
+		echo "$scriptName : Linear deployment activity ($AUTOMATIONHELPER/execute.sh $SOLUTION $BUILDNUMBER $PROJECT build.tsk) failed! Returned $exitCode"
 		exit $exitCode
 	fi
 fi
@@ -148,20 +193,17 @@ if [ -f "dirListFile" ]; then
 fi
 
 if [ -f "projectListFile" ]; then
-	echo "$0 :   Projects to process :"
-	echo
+	echo "$scriptName :   Projects to process :"; echo
 	cat projectListFile
 
 	while read PROJECT
 	do
 		
-		echo
-		echo "$0 : --- BUILD $PROJECT ---"
-		echo
+		echo; echo "$scriptName : --- BUILD $PROJECT ---"; echo
 		cd $PROJECT
 		exitCode=$?
 		if [ $exitCode -ne 0 ]; then
-			echo "$0 : cd $PROJECT failed! Exit code = $exitCode."
+			echo "$scriptName : cd $PROJECT failed! Exit code = $exitCode."
 			exit $exitCode
 		fi
 
@@ -176,7 +218,7 @@ if [ -f "projectListFile" ]; then
 			./build.sh "$SOLUTION" "$BUILDNUMBER" "$BUILDENV" "$ACTION"
 			exitCode=$?
 			if [ $exitCode -ne 0 ]; then
-				echo "$0 : $PROJECT Build Failed, exit code = $exitCode."
+				echo "$scriptName : $PROJECT Build Failed, exit code = $exitCode."
 				exit $exitCode
 			fi
 			
@@ -185,7 +227,7 @@ if [ -f "projectListFile" ]; then
 			../$AUTOMATIONHELPER/execute.sh "$SOLUTION" "$BUILDNUMBER" "$BUILDENV" "build.tsk" "$ACTION" 2>&1
 			exitCode=$?
 			if [ $exitCode -ne 0 ]; then
-				echo "$0 : Linear deployment activity ($AUTOMATIONHELPER/execute.sh $SOLUTION $BUILDNUMBER $PROJECT build.tsk) failed! Returned $exitCode"
+				echo "$scriptName : Linear deployment activity ($AUTOMATIONHELPER/execute.sh $SOLUTION $BUILDNUMBER $PROJECT build.tsk) failed! Returned $exitCode"
 				exit $exitCode
 			fi
 		fi
@@ -198,7 +240,7 @@ if [ -f "projectListFile" ]; then
 	
 	if [ -z $lastProject ]; then
 		echo
-		echo "$0 : No projects found containing build.sh, no build action attempted."
+		echo "$scriptName : No projects found containing build.sh, no build action attempted."
 		echo
 	fi
 	
@@ -207,8 +249,6 @@ if [ -f "projectListFile" ]; then
 
 else
 
-	echo
-	echo "$0 : No projects found, no build action attempted."
-	echo
+	echo; echo "$scriptName : No projects found, no build action attempted."; echo
 
 fi
--- automation/buildandpackage/packageCopyArtefacts.sh
+++ automation/buildandpackage/packageCopyArtefacts.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 set -e
+scriptName=$(basename $0)
 
 # Arguments are not validated in sub-scripts, only at entry point
 DRIVER=$1
 WORK_DIR_DEFAULT=$2
 
 if [ -f  "$DRIVER" ]; then
-	echo; echo "$0 : Copy artifacts defined in $DRIVER"; echo
+	echo; echo "$scriptName : Copy artifacts defined in $DRIVER"; echo
 	config=$(cat ${DRIVER}) # cat will read all lines, native READ will miss lines that done have line-feed
 	while read -r line; do
 
 		#deleting lines starting with # ,blank lines ,lines with only spaces
 		ARTIFACT=$(sed -e 's/#.*$//' -e '/^ *$/d' <<< $line)
 
-		if [ ! -z $ARTIFACT ]; then
+		if [ ! -z "$ARTIFACT" ]; then
 			# There must be a more elegant way to do this, but the current implementation is to overcome variable expansion when containing / character(s)
 			declare -a artArray=${ARTIFACT};
 			x=0
@@ -61,7 +62,7 @@ if [ -f  "$DRIVER" ]; then
 			cp $copyParent -av $source $targetPath
 			exitCode=$?
 			if [ $exitCode -ne 0 ]; then
-				echo "$0 : cp -v ../$ARTIFACT . failed! Exit code = $exitCode."
+				echo "$scriptName : cp -v ../$ARTIFACT . failed! Exit code = $exitCode."
 				exit $exitCode
 			fi
 			
@@ -71,7 +72,7 @@ if [ -f  "$DRIVER" ]; then
 	done < <(echo "$config")
 
 	if [ -z "$artefactList" ]; then
-		echo "$0 :   [WARN] No artefacts processed from definition file ($DRIVER)."
+		echo "$scriptName :   [WARN] No artefacts processed from definition file ($DRIVER)."
 	fi
 			
 fi
